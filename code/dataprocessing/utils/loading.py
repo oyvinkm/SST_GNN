7d40e9da34d7b0113069f61bccadc6f18ffd66fc
oyvinkm <phk851@alumni.ku.dk>
2024-02-13 12:22:43 +0100

    Implemented changes to model to handle multiple trajectories

--- code/dataprocessing/utils/loading.py
+++ code/dataprocessing/utils/loading.py
@@ -42,6 +42,81 @@ def load_preprocessed(args):
   return train_loader, test_loader, stats_list
 
 
+def load_trajectories(filename, trajectories, save = False, save_folder = None):
+  """
+  Loads no_trajectories from h5py file
+  file : either, test, train or valid
+  """
+  dataset_dir = os.path.join(os.getcwd(), 'data/cylinder_flow')
+  if filename not in ['test', 'train', 'valid']:
+     filename = 'test'
+  datafile = os.path.join(dataset_dir + f'/{filename}.h5')
+  #Define the list that will return the data graphs
+  data_list = []
+
+  #define the time difference between the graphs
+  dt=0.01   #A constant: do not change!
+
+  #define the number of trajectories and time steps within each to process.
+  #note that here we only include 2 of each for a toy example.
+  number_ts = 600
+  with h5py.File(datafile, 'r') as data:
+    
+    for trajectory in trajectories:
+        print("Trajectory: ",trajectory)
+
+        #We iterate over all the time steps to produce an example graph except
+        #for the last one, which does not have a following time step to produce
+        #node output values
+        for ts in range(len(data[trajectory]['velocity'])-1):
+            if(ts==number_ts):
+                break
+
+            #Get node features
+
+            #Note that it's faster to convert to numpy then to torch than to
+            #import to torch from h5 format directly
+            momentum = torch.tensor(np.array(data[trajectory]['velocity'][ts]))
+                
+            #node_type = torch.tensor(np.array(data[trajectory]['node_type'][ts]))
+            tmp = tf.convert_to_tensor(data[trajectory]['node_type'][0])
+            node_type = torch.tensor(np.array(tf.one_hot(tf.convert_to_tensor(data[trajectory]['node_type'][0]), NodeType.SIZE))).squeeze(1)
+            x = torch.cat((momentum,node_type),dim=-1).type(torch.float)
+            if ts == 0: print(f'Num nodes trajectory {trajectory} : {x.shape[0]}')
+
+            #Get edge indices in COO format
+            edges = triangles_to_edges(tf.convert_to_tensor(np.array(data[trajectory]['cells'][ts])))
+
+            edge_index = torch.cat( (torch.tensor(edges[0].numpy()).unsqueeze(0) ,
+                        torch.tensor(edges[1].numpy()).unsqueeze(0)), dim=0).type(torch.long)
+
+            #Get edge features
+            u_i=torch.tensor(np.array(data[trajectory]['pos'][ts]))[edge_index[0]]
+            u_j=torch.tensor(np.array(data[trajectory]['pos'][ts]))[edge_index[1]]
+            u_ij=u_i-u_j
+            u_ij_norm = torch.norm(u_ij,p=2,dim=1,keepdim=True)
+            edge_attr = torch.cat((u_ij,u_ij_norm),dim=-1).type(torch.float)
+
+            #Node outputs, for training (velocity)
+            v_t=torch.tensor(np.array(data[trajectory]['velocity'][ts]))
+            v_tp1=torch.tensor(np.array(data[trajectory]['velocity'][ts+1]))
+            y=((v_tp1-v_t)/dt).type(torch.float)
+
+            #Node outputs, for testing integrator (pressure)
+            p=torch.tensor(np.array(data[trajectory]['pressure'][ts]))
+
+            #Data needed for visualization code
+            cells=torch.tensor(np.array(data[trajectory]['cells'][ts]))
+            mesh_pos=torch.tensor(np.array(data[trajectory]['pos'][ts]))
+            w = x.new_ones(x.shape[0], 1)
+            data_list.append(Data(x=x, edge_index=edge_index, edge_attr=edge_attr,y=y,p=p,
+                                  cells=cells, weights = w, mesh_pos=mesh_pos, t=ts))
+        if save:
+          file = f'trajectory_{trajectory}'
+          save_data_list(data_list, file, save_folder)
+          data_list = []
+  return data_list
+
 
 def loadh5py(filename, no_trajectories = 1, save = False, save_folder = None):
   """
@@ -117,7 +192,7 @@ def loadh5py(filename, no_trajectories = 1, save = False, save_folder = None):
               data_list.append(Data(x=x, edge_index=edge_index, edge_attr=edge_attr,y=y,p=p,
                                     cells=cells, weights = w, mesh_pos=mesh_pos, t=ts))
           if save:
-            file = f'trajectory_{i}'
+            file = f'trajectory_{trajectory}'
             save_data_list(data_list, file, save_folder)
             data_list = []
   return data_list
