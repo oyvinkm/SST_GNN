cf473ca0abee7bbcb591cbba352d2e1c5ea08d36
oyvinkm <phk851@alumni.ku.dk>
2024-02-27 12:10:38 +0100

    Edge attributes as node attributes processing

--- code/create_node_traj.py
+++ code/create_node_traj.py
@@ -1,9 +1,11 @@
-from dataprocessing.utils.loading import constructDatasetFolders, find_trajectory_nodes
+from dataprocessing.utils.loading import constructDatasetFolders, find_trajectory_nodes, extend_node_attributes
 from loguru import logger
 if __name__ == "__main__":
     logger.success('Finding trajectories')
     find_trajectory_nodes()
     logger.success('Constructing Dataset Folders train/test/val/')
     constructDatasetFolders(same_nodes = 'same_nodes.json', choose='max')
+    logger.success('Extending node attributes for train/test/val')
+    extend_node_attributes()
     logger.success('Done constructing <3')
 
